d6a220aeefc939bb1142e720957be93cfcf4fcec
Jenipha Rachel <35962711+jenipharachel@users.noreply.github.com>
2019-11-04 14:31:28 +0530

    Add files via upload

--- Sample_app/app/app.js
+++ Sample_app/app/app.js
@@ -2,27 +2,39 @@ $(document).ready(function() {
   app.initialized().then(function(_client) {
     var client = _client;
     client.events.on("app.activated", function() {
-      $("#submit").on("click", function(e) {
+      client.iparams.get().then(
+        function(data) {
+          console.log(data);
+        },
+        function(error) {
+          console.log(error);
+          // failure operation
+        }
+      );
+
+      /*$("#submit").on("click", function(e) {
         e.preventDefault();
         var name = $("#name").val();
         var age = $("#age").val();
         console.log(name, age);
         $("#form").hide();
         $("p").text("Welcome," + name + ". Your age is" + age);
-      });
-      //   client.iparams.get().then(
-      //     function(data) {
-      //       console.log(data);
-      //       var dt = new Date();
-      //       var year = dt.getFullYear();
-      //       console.log(year);
-      //       $("#apptext").text("Year:" + (year - data.Age));
-      //     },
-      //     function(error) {
-      //       console.log(error);
-      //       // failure operation
-      //     }
-      //   );
+      });*/
+
+      /*client.iparams.get().then(
+          function(data) {
+            console.log(data);
+            var dt = new Date();
+            var year = dt.getFullYear();
+            console.log(year);
+            $("#apptext").text("Year:" + (year - data.Age));
+          },
+          function(error) {
+            console.log(error);
+            // failure operation
+          }
+        ); */
+
       // client.data.get('contact')
       //     .then(function(data) {
       //         $('#apptext').text("Ticket created by " + data.contact.name);
